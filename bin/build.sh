d8ff0323b4005893a3d3ee965849e4335c8ff183
Kevin Woo <kevinawoo@gmail.com>
2019-03-01 17:08:53 -0800

    feat(kube): provide deployment instead of pod

--- bin/build.sh
+++ bin/build.sh
@@ -4,11 +4,11 @@ cd "$(dirname "$0")/.."
 source bin/env
 docker build -t armory/debugging-tools:${DOCKER_TAG} .
 
-# spit out a new kube-pod spec
-cat << EOF > kube-pod.yml
-# WARNING, this is rendered using templates/kube-pod.yml
+# spit out a new deployment spec
+cat << EOF > deployment.yml
+# WARNING, this is rendered using templates/deployment.yml
 # any changes here will not be reflected on the next build
 
 EOF
 
-cat templates/kube-pod.yml | sed 's@$DOCKER_TAG@'"$DOCKER_TAG"'@' >> kube-pod.yml
+cat templates/deployment.yml | sed 's@$DOCKER_TAG@'"$DOCKER_TAG"'@' >> deployment.yml
